8aa24a3bb96340478428c3aa9123b91faf9a33b3
Wyatt <wyatt.burkhardt@gmail.com>
2018-08-05 16:33:10 -0400

    navbar skeleton created

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  navLinks: any[] = [
+    {
+      text: 'Home',
+      link: '#'
+    },
+    {
+      text: 'Search',
+      link: '#'
+    },
+    {
+      text: 'Library',
+      link: '#'
+    }
+  ];
 
   constructor() { }
 
